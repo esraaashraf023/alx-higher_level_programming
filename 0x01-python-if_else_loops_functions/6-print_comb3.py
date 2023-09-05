1f9b925773f67ad9f664ee9f11bcc448bc7a35d9
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-05 15:59:52 -0700

    add file6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{:d}".format(i))
+        print('{:d}'.format(i))
     else:
-        print("{:d}".format(i), end=", "
+        print('{:d}'.format(i), end=', ')
