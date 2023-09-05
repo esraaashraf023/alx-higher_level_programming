084ae5c1d16a4c8119da64b3374e2019370bd828
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-05 15:35:39 -0700

    add file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for k in range (100):
     if k == 99:
         print("{:d}".format(k))
     else:
-        print("{:d}".format(k).format(k).zfill(2), end=' , ')
+        print("{:d}".format(k).zfill(2), end=' , ')
