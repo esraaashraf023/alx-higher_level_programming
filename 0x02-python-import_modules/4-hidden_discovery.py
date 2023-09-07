810fd7de2e1ccb1adc5282e5f02ef8a5c14f9dcf
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-07 15:19:04 -0700

    Task3

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
+
 if _name_ == "_main_":
     import hidden_4
 
     names = dir(hidden_4)
     for name in names:
-        if name[0:2]="__":
+        if not name.startswith("__"):
             print(name)
