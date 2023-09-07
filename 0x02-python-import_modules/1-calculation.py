0061f98fbacc134630908d79df0f46d90c046f1d
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-07 13:57:37 -0700

    arithmetic operatrs

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ if __name__ == "__main__":
     from calculator_1.py import add, sub, mul, div
 a = 10
 b = 5
-r = add(a, b)
-    print('{:d} + {:d} = {:d}'.format(a, b, r))
-    r = sub(a, b)
-     print('{:d} - {:d} = {:d}'.format(a, b, r))
-     r = mul(a, b)
-      print('{:d} * {:d} = {:d}'.format(a, b, r))
-      r = div(a, b)
-       print('{:d} / {:d} = {:d}'.format(a, b, r))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+     print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+      print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+       print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
