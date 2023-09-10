3e6b120462295e9c3725ff82169ede0af14f8995
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-09 19:51:32 -0700

    Add Max file9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    i = my_list[0]
+    for element in my_list:
+        if element > i:
+            i = element
+    return i
