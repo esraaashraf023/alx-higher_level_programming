972772c3dd55d2ba562b87662fbe0559ba25160e
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-09 18:27:03 -0700

    TESKE

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        print(' '.Join('{:d}'.format(J) for J in i))
+        print(' '.join('{:d}'.format(J) for J in i))
