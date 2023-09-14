af50f4af6da1252f17c548c9807082a0703650ec
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-13 18:50:59 -0700

    Add file 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     new_dictionary = a_dictionary.copy()
     for key in new_dictionary:
         new_dictionary[key] *= 2
-    return(new_dictionary)
+    return new_dictionary
