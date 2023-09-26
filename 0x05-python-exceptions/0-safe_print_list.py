5cb8ddf1dd11972c2addc2e28adfa77f32314a65
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-25 17:41:14 -0700

    add file 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     n = 0
-    for i in range |(x):
+    for i in range(x):
         try:
             print(my_list[i], end='')
             n +=1
-        except:     
+        except:  
             break
     print()
     return n
