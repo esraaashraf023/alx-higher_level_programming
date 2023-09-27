215fdf5714be9870f451383b9eacca0723aa7cb8
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-27 12:08:45 -0700

    add first file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a Square Class"""
+
+
+class Square:
+    """This is Class Square"""
+    def__init__(self, size):
+        """Instantiation a Square give size"""
+        self.__size = size
