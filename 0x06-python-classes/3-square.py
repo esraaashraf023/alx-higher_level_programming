6b6a75fe09fa4793ea7ce7e4001d18b779243360
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-27 12:59:30 -0700

    add file3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""that defines a square"""
+
+
+class Square:
+    """ only squares integers """
+
+    def __init__(self, size=0):
+        """ Exeptions to not integers """
+        if int(size) > -1 and size == int(size):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+            raise ValueError("size must be >= 0")
+
+        def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
