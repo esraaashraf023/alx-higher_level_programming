4bc8a67ba2f956bffe0c4c45c473b67b0fd2329d
esraaashraf023 <esraaashraf780567@gmail.com>
2023-09-27 12:39:12 -0700

    add file 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Squares:
     """a Class Square"""
     def __init__(self, size=0):
         """Instantiation a square"""
-        if int(self, size) > -1 and size == int(size):
+        if int(size) > -1 and size == int(size):
             self.__size = size
         else:
             raise TypeError("size must be an integer")
