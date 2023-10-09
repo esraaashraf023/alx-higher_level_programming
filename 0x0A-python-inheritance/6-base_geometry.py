e51c327522aa08712c02ac154d4913568b09e5ab
esraaashraf023 <esraaashraf780567@gmail.com>
2023-10-09 15:41:47 -0700

    add test 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A base class"""
+
+    def area(self):
+        """The geometric shape"""
+        raise Exception("area() is not implemented")
