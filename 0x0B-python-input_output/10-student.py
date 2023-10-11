5ce11555424f82edbaea15963c2c3cc443ae0177
esraaashraf023 <esraaashraf780567@gmail.com>
2023-10-11 15:46:44 -0700

    add task10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class Student that defines a student by"""
+
+
+class Student:
+    """Define class"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization of class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
