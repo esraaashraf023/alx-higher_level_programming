f99f56d97c9409b425a8eb7727861342e3659060
esraaashraf023 <esraaashraf780567@gmail.com>
2023-10-11 16:09:52 -0700

    add task 12

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,21 +3,22 @@
 
 
 class Student:
-     """Define class"""
+    """Define class"""
+
     def __init__(self, first_name, last_name, age):
+        """Initialization of class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-         """Retrieves a dictionary representation of a Student instance"""
+        """Retrieves a dictionary representation of a Student instance"""
         if (type(attrs) == list and
                 all(type(element) == str for element in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student"""
-        for key, value in json.items():
-            setattr(self, key, value)
-
+        """Replaces all attributes of the Student instance"""
+        for key, val in json.items():
+            setattr(self, key, val)
