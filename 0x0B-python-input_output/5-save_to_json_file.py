ef16f468f2ce72115013e9f121dbbd4754be94d4
esraaashraf023 <esraaashraf780567@gmail.com>
2023-10-11 15:18:17 -0700

    add task5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@
 import json
 
 
-def load_from_json_file(filename):
+def save_to_json_file(my_obj, filename):
     """that writes an Object to a text file"""
-    with open(filename, 'r') as file:
-        return json.load(file)
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
