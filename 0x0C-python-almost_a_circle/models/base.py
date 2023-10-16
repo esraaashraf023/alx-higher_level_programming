c39c4d23e63aa0d1fe5f7bb680ed5f4c90dac0ab
esraaashraf023 <esraaashraf780567@gmail.com>
2023-10-16 11:35:56 -0700

    add file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
     """class Base"""
     __nb_objects = 0
 
-    def __init__(self, id=None)::
+    def __init__(self, id=None):
         """class `constructor`"""
         if id is not None:
             self.id = id
