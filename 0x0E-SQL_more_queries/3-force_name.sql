aa1021eda344eb452b9763d1fb25cab3a1c3df48
esraaashraf023 <esraaashraf780567@gmail.com>
2023-11-15 11:31:14 -0800

    add file 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- Creates the table force_name on MySQL server.
+-- id INT
+-- name VARCHAR(256) can’t be null
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
