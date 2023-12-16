f1b90f69a6ad6e808e48eb29b88041fb0d2e975d
esraaashraf023 <esraaashraf780567@gmail.com>
2023-12-16 06:42:23 -0800

    UPdata

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+
     # connect to SQL databas.
     db = MySQLdb.connect(host="localhost", port=3306,
-                        user=username, passwd=password, db=database)
+            user=username, passwd=password, db=database)
 
     cursor = db.cursor()
 
