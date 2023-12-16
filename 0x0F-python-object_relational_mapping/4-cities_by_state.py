97d839aafdc43d0d023fa99877ac29c4a5180423
esraaashraf023 <esraaashraf780567@gmail.com>
2023-12-16 13:20:12 -0800

    UPDATA

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Module to retrieve and display cities from a MySQL database"""
 import MySQLdb
 import sys
@@ -10,7 +9,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to MySQL..
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host='localhost', user=username,
         passwd=password, db=database, port=3306
@@ -18,17 +17,17 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        INNER JOIN states s ON c.state_id = s.id
-        ORDER BY cities.id ASC.
-    """
+    query = """SELECT cities.id, cities.name, states.name
+                 FROM states
+                 INNER JOIN cities ON states.id = cities.state_id
+                 ORDER BY cities.id ASC"""
+    # Execute the query..
     cursor.execute(query)
 
-    rows = cursor.fetchall()
+    # Fetch all the results
+    results = cursor.fetchall()
 
-    for row in rows:
+    for row in results:
         print(row)
 
     cursor.close()
