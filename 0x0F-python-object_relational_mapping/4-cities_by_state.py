11b585d37609bacaadef48da643a87899e6b0e9a
esraaashraf023 <esraaashraf780567@gmail.com>
2023-12-16 12:52:57 -0800

    add file 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module to retrieve and display cities from a MySQL database"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Retrieve command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL..
+    db = MySQLdb.connect(
+        host='localhost', user=username,
+        passwd=password, db=database, port=3306
+    )
+
+    cur = db.cursor()
+
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states s ON c.state_id = s.id
+        ORDER BY cities.id ASC.
+    """
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
