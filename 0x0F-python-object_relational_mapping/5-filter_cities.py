91364b81424ae7d1b49d0f342f9975c79aabdadd
esraaashraf023 <esraaashraf780567@gmail.com>
2023-12-16 14:03:31 -0800

    TASK5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""that takes in the name of a state as an argument and lists all cities"""
+"""
+Script to retrieve and display cities of a state from a MySQL database
+"""
+
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     # Retrieve command-line arguments
@@ -10,7 +14,7 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to MySQL.
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host='localhost', user=username,
         passwd=password, db=database, port=3306
@@ -18,14 +22,13 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
+    # Create the SQL
     query = """SELECT cities.name FROM cities
-                 FROM states
-                 INNER JOIN cities ON states.id = cities.state_id
-                 ORDER BY cities.id ASC"""
-    # Execute the query..
-    cursor.execute(query, (state_name,))
+               JOIN states ON cities.state_id = states.id
+               WHERE states.name = %s ORDER BY cities.id ASC"""
 
-    # Fetch all the results
+    # Execute the query.
+    cursor.execute(query, (state_name,))
     results = cursor.fetchall()
 
     cities = [row[0] for row in results]
