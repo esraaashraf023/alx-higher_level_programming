feabab5b1c527501f5174a2e858ceb348b0ba0dd
esraaashraf023 <esraaashraf780567@gmile.com>
2024-01-08 00:42:32 +0200

    add file 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This script sends a request to a URL..."""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+
+    print(x_request_id)
