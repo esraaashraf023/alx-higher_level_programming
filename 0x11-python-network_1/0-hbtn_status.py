763eb7ad0ae827d08af27851935f9354598aa932
esraaashraf023 <esraaashraf780567@gmile.com>
2024-01-08 00:07:40 +0200

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,8 @@
 Write a script that fetches https://alx-intranet.hbtn.io/status.
 """
 import urllib.request
-if __niame__ == '__main__':
+
+if __name__ == '__main__':
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         content = response.read()
