f0133d21fb13ee91324dc488ba6b333bd8009058
esraaashraf023 <esraaashraf780567@gmile.com>
2024-01-08 00:32:51 +0200

    add file3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script sends a request to a URL.."""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as error:
+        print(f"Error code: {e.code}")
