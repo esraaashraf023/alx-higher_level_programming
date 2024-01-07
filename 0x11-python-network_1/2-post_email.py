cfa3c436058da9d5e03613d9eefb5b1a9f20fbdb
esraaashraf023 <esraaashraf780567@gmile.com>
2024-01-08 00:24:37 +0200

    add file3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,13 +7,10 @@ import urllib.request
 import sys
 
 if __name__ == '__main__':
+
     url = sys.argv[1]
     email = sys.argv[2]
-
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    request = urllib.request.Request(url, data)
 
-    with urllib.request.urlopen(request) as response:
-        content = response.read().decode('utf-8')
-
-    print(content)
+    with urllib.request.urlopen(url, data=data) as response:
+        print(response.read().decode("utf-8"))
