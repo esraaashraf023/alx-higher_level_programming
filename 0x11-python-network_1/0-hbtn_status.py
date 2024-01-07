5521657eba2cf7131af02219f2eb5f7514652cb8
esraaashraf023 <esraaashraf780567@gmile.com>
2024-01-07 23:53:38 +0200

    add file0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write a script fetches https://alx-intranet.hbtn.io/status.
+"""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+
+    print("Body response:")
+    print(f"\t- type:{type(content)}")
+    print(f"\t- content:{(content")
+    print(f"\t- utf8 content:(content.decode('UTF-8')"))
