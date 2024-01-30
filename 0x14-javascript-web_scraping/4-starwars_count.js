902874b76de0a9b00206d06f92894387e6b76294
esraaashraf023 <esraaashraf780567@gmail.com>
2024-01-31 01:31:58 +0200

    UPdate

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = process.argv[2];
-const characterId = 18;
 
-request(apiUrl, (error, response, body) => {
-  if (error) console.error(error);
-  const films = JSON.parse(body).results;
-  const count = films.filter(film => {
-    return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
-  }).length;
-  console.log(count);
+request.get(process.argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  const filmsWithWedge = data.results.filter(film => film.characters.find(char => char.includes('18')));
+  console.log(filmsWithWedge.length);
 });
