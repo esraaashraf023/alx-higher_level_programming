5f5cd9f8ff6ac3aa45448d801720960024cdf866
esraaashraf023 <esraaashraf780567@gmail.com>
2024-01-31 02:22:21 +0200

    update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, res, body) => {
 	if (err) {
 		return console.log(err);
